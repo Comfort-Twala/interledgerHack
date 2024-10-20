3b97110b688dc75b284b7ed6bb18ad23d9e8a23f
Daniel-Ind <danieljounior@gmail.com>
2024-10-20 02:04:36 +0200

    Added history routes

---
+++ src/app/api/routes/historyRoutes.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { sendResponse } from "../utils/response";
+import HistoryService from "../services/HistoryService";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    if (id) {
+        try {
+            const history = await HistoryService.getHistoryById(parseInt(id));
+            return sendResponse(history, 200, null);
+        } catch (error) {
+            return sendResponse(null, 500, 'Internal Server Error');
+        }
+    }
+
+    try {
+        const histories = await HistoryService.getHistories();
+        return sendResponse(histories, 200, null);
+    } catch (error) {
+        return sendResponse(null, 500, 'Internal Server Error');
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const history = await request.json();
+        const createdHistory = await HistoryService.createHistory(history);
+        return sendResponse(createdHistory, 201, null);
+    } catch (error) {
+        return sendResponse(null, 400, 'Failed to create history');
+    }
+}
