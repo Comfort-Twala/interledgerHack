741a323b9a6ac0d5531c0367b6eaed0352098398
Daniel-Ind <danieljounior@gmail.com>
2024-10-20 02:05:32 +0200

    Added wallet routes

---
+++ src/app/api/routes/walletRoutes.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { sendResponse } from "../utils/response";
+import WalletService from "../services/walletService";
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    if (id) {
+        try {
+            const wallet = await WalletService.getWalletById(parseInt(id));
+            return sendResponse(wallet, 200, null);
+        } catch (error) {
+            return sendResponse(null, 500, 'Internal Server Error');
+        }
+    }
+
+    try {
+        const wallets = await WalletService.getWallets();
+        return sendResponse(wallets, 200, null);
+    } catch (error) {
+        return sendResponse(null, 500, 'Internal Server Error');
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const wallet = await request.json();
+        const createdWallet = await WalletService.createWallet(wallet);
+        return sendResponse(createdWallet, 201, null);
+    } catch (error) {
+        return sendResponse(null, 400, 'Failed to create wallet');
+    }
+}
+
+export async function PUT(request: Request) {
+    const { id } = await request.json();
+    try {
+        const walletData = await request.json();
+        const updatedWallet = await WalletService.updateWallet(id, walletData);
+        return sendResponse(updatedWallet, 200, null);
+    } catch (error) {
+        return sendResponse(null, 400, 'Failed to update wallet');
+    }
+}
